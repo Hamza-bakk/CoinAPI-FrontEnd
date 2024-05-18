9b67a6a857cdb5c42a1edaa3532648f00125a23c
Hamza-bakk <bakkar.hamza92@gmail.com>
2024-05-18 14:38:23 +0200

    gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx,scss}"],
+  theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'min': '501px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '500px'},
+      // => @media (max-width: 639px) { ... }
+    }
+  },
+  plugins: [],
+};
